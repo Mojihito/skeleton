8ab0fa90bee1b4f71b1053dcae5f54d5980b4f88
Mojihito <tpiasecki85@gmail.com>
2020-04-02 14:23:50 +0200

    Added view with simple form where user can input repository owner and repository name as variable to call github API for issues

--- src/Controller/KanbanController.php
+++ src/Controller/KanbanController.php
@@ -1,25 +1,55 @@
 <?php
 
-// src/Controller/LuckyController.php
 namespace App\Controller;
 
 use App\Services\GitHubService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 
 class KanbanController extends AbstractController
 {
-    public function showKanbanBoard(GitHubService $gitHubService)
+    public function showKanbanBoard(Request $request, GitHubService $gitHubService)
     {
-
+        $owner = $request->get('owner');
+        $repository = $request->get('repository');
+        $gitHubService->setGithubOwner($owner);
+        $gitHubService->setRepository($repository);
         $milestones = $gitHubService->getAllMilestones();
         foreach ($milestones as $key => $milestone){
-            $milestones[$key]['openedIssues'] = $gitHubService->getIssuesByStatusForMilestone('open' , $milestone['number']);
-            $milestones[$key]['closedIssues'] = $gitHubService->getIssuesByStatusForMilestone('closed' , $milestone['number']);
+            $milestones[$key]['openedIssues'] = $gitHubService->getIssuesByStatusForMilestone($gitHubService::STATUS_OPENED_ISSUES , $milestone['number']);
+            $milestones[$key]['closedIssues'] = $gitHubService->getIssuesByStatusForMilestone($gitHubService::STATUS_CLOSED_ISSUES , $milestone['number']);
         }
 
         return $this->render('view.html.twig', [
             'milestones' => $milestones
+        ]);
+    }
+
+    public function chooseRepo(Request $request)
+    {
+        $form = $this->createFormBuilder()
+            ->add('owner', TextType::class, [
+                'label' => 'Owner of the repository',
+            ])
+            ->add('repository', TextType::class, [
+                'label' => 'Repository name',
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Pick',
+            ])
+            ->getForm();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            return $this->redirectToRoute('app_show_kanban_board',$data);
+        }
+
+        return $this->render('chose_repo.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
